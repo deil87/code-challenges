dfcef1430ba11c65b2303fbc50157b02af31d477
deil <deil_87@mail.ru>
2016-07-15 19:22:43 +0300

    Introduced Tieu and Customer actors

--- src/main/scala/com/example/PizzaRestaurant.scala
+++ src/main/scala/com/example/PizzaRestaurant.scala
@@ -2,11 +2,22 @@ package com.example
 
 import akka.actor.ActorSystem
 
+import scala.util.Random
+
 object PizzaRestaurant extends App {
   val system = ActorSystem("PizzaRestaurantSystem")
-  val pingActor = system.actorOf(PingActor.props, "pingActor")
-  pingActor ! PingActor.Initialize
-  // This example app will ping pong 3 times and thereafter terminate the ActorSystem - 
-  // see counter logic in PingActor
+
+  val numberOfCustomers = 5
+
+  val customersMeta = (1 to numberOfCustomers)
+    .map(i => CustomersMeta(i, Random.nextInt(100), orderEstimate = Random.nextInt(8) + 2))
+    .sortBy(_.arrivalTime).toList
+
+  val tieuActor = system.actorOf(TieuActor.props, "tieuActor")
+
+  customersMeta.foreach(cm => system.actorOf(CustomerActor.props(cm.id, cm.orderEstimate), "customerActor"+ cm.id))
+
   system.awaitTermination()
 }
+
+case class CustomersMeta(id: Int, arrivalTime: Int, orderEstimate: Int)
