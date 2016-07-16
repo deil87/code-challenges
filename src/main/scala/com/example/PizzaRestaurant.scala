8ee54a92a7241d2bbfb2313538a609af0fdcfa73
deil <deil_87@mail.ru>
2016-07-16 18:10:11 +0300

    Performance issues revealed

--- src/main/scala/com/example/PizzaRestaurant.scala
+++ src/main/scala/com/example/PizzaRestaurant.scala
@@ -20,7 +20,7 @@ object PizzaRestaurant extends App {
   }
 
   def randomPizzaTimeCost: Int = {
-    Random.nextInt(1000000000) + 1
+    Random.nextInt(1000000) + 1
   }
 
 
@@ -29,7 +29,7 @@ object PizzaRestaurant extends App {
 
 //  customersMeta.foreach(cm => system.actorOf(CustomerActor.props(cm.id, cm.orderEstimate), "customerActor"+ cm.id))
 
-  val customersMetaPredef1 = List(CustomersMeta(1, 0, 3), CustomersMeta(2, 1, 9), CustomersMeta(3, 2, 6))
+  val customersMetaPredef1 = List(CustomersMeta(0, 3), CustomersMeta(1, 9), CustomersMeta(2, 5))
 
   var totalMinWaiting = 0L
 //  println("Times: " + customersMeta.map(cm => cm.id + ":" + cm.arrivalTime.toString).mkString(", "))
@@ -43,7 +43,7 @@ object PizzaRestaurant extends App {
 
 //    println(accumulatorOfAllWaitingTimes)
 
-  restCustomers match {
+    alreadyAwaitingCustomers ::: restCustomers match {
       case Nil =>
 //        println(s"Serving last customer $currentCustomer")
         accumulatorOfAllWaitingTimes + currentWaitingTime
@@ -58,7 +58,7 @@ object PizzaRestaurant extends App {
         totalMinWaiting = totalMinWaiting + (finishTime - startTime)
 //        println(s"Customers arrived during serving pizza for ${currentCustomer.id}: ${customersArrivedDuringCurrent.mkString(",")}")
         val bestNext = customersAwaitingServing match {
-          case Nil => restCustomers.head
+          case Nil => futureCustomers.head
           case ca =>
             val withMinOrderEstimateImpactToOthers = ca.minBy(_.orderEstimate)
             val withCurrentMaxWaitingTime = ca.maxBy(_.waitingTime)
@@ -92,11 +92,11 @@ object PizzaRestaurant extends App {
 
   println(s"Min avg of waiting time is: ${minSum / customersMeta.length}")
 
-  /*val minSumPredef1 = calculateMinSumOfWaitingTime(Nil, customersMetaPredef1.head, customersMetaPredef1.tail)
+  val minSumPredef1 = calculateMinSumOfWaitingTime(0L, customersMetaPredef1.head,  Nil,customersMetaPredef1.tail)
 
-  println(s"Min sum of waiting time in customersMetaPredef1 is: $minSumPredef1")
+  println(s"==================================\nMin sum of waiting time in customersMetaPredef1 is: $minSumPredef1")
 
-  println(s"Min avg of waiting time in customersMetaPredef1 is: ${minSumPredef1 / customersMetaPredef1.length}")*/
+  println(s"Min avg of waiting time in customersMetaPredef1 is: ${minSumPredef1 / customersMetaPredef1.length}")
   system.shutdown()
 }
 
